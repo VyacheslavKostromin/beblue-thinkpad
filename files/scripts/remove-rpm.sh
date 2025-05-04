3e43d119755388c31af2d1aefb98ae651b39eb84
beb <beb_@tutamail.com>
2025-05-05 03:56:43 +0700

    try to remove rpm :trollface: 2

--- files/scripts/remove-rpm.sh
+++ files/scripts/remove-rpm.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
-rm -rf /etc/rpm
-rm -rf /usr/bin/rpm*
-rm -rf /usr/lib/11/450de0986730031b4eb2a9d73880385493a5ee
-rm -rf /usr/lib/.build-id/3b/c0d669d614e1325bd93cb27b3b574fd5d580be
-rm -rf /usr/lib/.build-id/55/1347b128caff3cdf2a33c653bfc397214637ba
-rm -rf /usr/lib/.build-id/9c/1d9fd6f3b7c5baa0cfded0824ad6d30df854b2
-rm -rf /usr/lib/.build-id/b0/03fbcb3bdeb14e7ee1f165cf16361b8d3ca82b
-rm -rf /usr/lib/rpm
-rm -rf /usr/lib/sysimage/rpm
-rm -rf /usr/lib/systemd/system/rpmdb-rebuild
-rm -rf /usr/share/doc/rpm
-rm -rf /usr/share/licenses/rpm
-rm -rf /usr/share/locale/*/LC_MESSAGES/rpm.mo
-rm -rf /usr/share/man/man8/rpm*
+
+(rpm -ql rpm; rpm -ql rpm-libs; rpm -ql rpm-sequoia) | xargs -I {} rm -rf {}
