adaeec190e8255ab52de9cec8051af75d313f76f
beb <beb_@tutamail.com>
2025-08-04 04:10:19 +0700

    mimalloc rollback

--- files/scripts/mimalloc-pam.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-sed -i -e '$a\LD_PRELOAD DEFAULT=libmimalloc.so' -e '/^LD_PRELOAD[[:space:]]/d' /etc/security/pam_env.conf
--- files/system/etc/profile.d/mimalloc.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/sh
-export LD_PRELOAD=libmimalloc.so
